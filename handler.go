081aad0f891f1b151a8149deb7dda41338b4b4ef
cubicdaiya <cubicdaiya@gmail.com>
2014-06-13 09:48:18 +0900

    organize json

--- handler.go
+++ handler.go
@@ -10,8 +10,7 @@ import (
 
 var newLineTerm bool = false
 
-type result struct {
-	// runtime
+type resultRuntime struct {
 	GoVersion    string `json:"go_version"`
 	GoOs         string `json:"go_os"`
 	GoArch       string `json:"go_arch"`
@@ -19,24 +18,37 @@ type result struct {
 	GoroutineNum int    `json:"goroutine_num"`
 	Gomaxprocs   int    `json:"gomaxprocs"`
 	CgoCallNum   int64  `json:"cgo_call_num"`
-	// memory
-	MemoryAlloc      uint64 `json:"memory_alloc"`
-	MemoryTotalAlloc uint64 `json:"memory_total_alloc"`
-	MemorySys        uint64 `json:"memory_sys"`
-	MemoryLookups    uint64 `json:"memory_lookups"`
-	MemoryMallocs    uint64 `json:"memory_mallocs"`
-	MemoryFrees      uint64 `json:"memory_frees"`
-	// heap
-	HeapAlloc    uint64 `json:"heap_alloc"`
-	HeapSys      uint64 `json:"heap_sys"`
-	HeapIdle     uint64 `json:"heap_idle"`
-	HeapInuse    uint64 `json:"heap_inuse"`
-	HeapReleased uint64 `json:"heap_released"`
-	HeapObjects  uint64 `json:"heap_objects"`
-	// gabarage collection
-	GcNext uint64 `json:"gc_next"`
-	GcLast uint64 `json:"gc_last"`
-	GcNum  uint32 `json:"gc_num"`
+}
+
+type resultMemory struct {
+	MemoryAlloc      uint64 `json:"alloc"`
+	MemoryTotalAlloc uint64 `json:"total_alloc"`
+	MemorySys        uint64 `json:"sys"`
+	MemoryLookups    uint64 `json:"lookups"`
+	MemoryMallocs    uint64 `json:"mallocs"`
+	MemoryFrees      uint64 `json:"frees"`
+}
+
+type resultHeap struct {
+	HeapAlloc    uint64 `json:"alloc"`
+	HeapSys      uint64 `json:"sys"`
+	HeapIdle     uint64 `json:"idle"`
+	HeapInuse    uint64 `json:"inuse"`
+	HeapReleased uint64 `json:"released"`
+	HeapObjects  uint64 `json:"objects"`
+}
+
+type resultGc struct {
+	GcNext uint64 `json:"next"`
+	GcLast uint64 `json:"last"`
+	GcNum  uint32 `json:"num"`
+}
+
+type result struct {
+	Rr resultRuntime `json:"runtime"`
+	Rm resultMemory  `json:"memory"`
+	Rh resultHeap    `json:"heap"`
+	Rg resultGc      `json:"gc"`
 }
 
 func NewLineTermEnabled() {
@@ -52,31 +64,36 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	runtime.ReadMemStats(&mem)
 
 	result := &result{
-		GoVersion:    runtime.Version(),
-		GoOs:         runtime.GOOS,
-		GoArch:       runtime.GOARCH,
-		CpuNum:       runtime.NumCPU(),
-		GoroutineNum: runtime.NumGoroutine(),
-		Gomaxprocs:   runtime.GOMAXPROCS(0),
-		CgoCallNum:   runtime.NumCgoCall(),
-		// memory
-		MemoryAlloc:      mem.Alloc,
-		MemoryTotalAlloc: mem.TotalAlloc,
-		MemorySys:        mem.Sys,
-		MemoryLookups:    mem.Lookups,
-		MemoryMallocs:    mem.Mallocs,
-		MemoryFrees:      mem.Frees,
-		// heap
-		HeapAlloc:    mem.HeapAlloc,
-		HeapSys:      mem.HeapSys,
-		HeapIdle:     mem.HeapIdle,
-		HeapInuse:    mem.HeapInuse,
-		HeapReleased: mem.HeapReleased,
-		HeapObjects:  mem.HeapObjects,
-		// gabarage collection
-		GcNext: mem.NextGC,
-		GcLast: mem.LastGC,
-		GcNum:  mem.NumGC,
+		Rr: resultRuntime{
+			GoVersion:    runtime.Version(),
+			GoOs:         runtime.GOOS,
+			GoArch:       runtime.GOARCH,
+			CpuNum:       runtime.NumCPU(),
+			GoroutineNum: runtime.NumGoroutine(),
+			Gomaxprocs:   runtime.GOMAXPROCS(0),
+			CgoCallNum:   runtime.NumCgoCall(),
+		},
+		Rm: resultMemory{
+			MemoryAlloc:      mem.Alloc,
+			MemoryTotalAlloc: mem.TotalAlloc,
+			MemorySys:        mem.Sys,
+			MemoryLookups:    mem.Lookups,
+			MemoryMallocs:    mem.Mallocs,
+			MemoryFrees:      mem.Frees,
+		},
+		Rh: resultHeap{
+			HeapAlloc:    mem.HeapAlloc,
+			HeapSys:      mem.HeapSys,
+			HeapIdle:     mem.HeapIdle,
+			HeapInuse:    mem.HeapInuse,
+			HeapReleased: mem.HeapReleased,
+			HeapObjects:  mem.HeapObjects,
+		},
+		Rg: resultGc{
+			GcNext: mem.NextGC,
+			GcLast: mem.LastGC,
+			GcNum:  mem.NumGC,
+		},
 	}
 
 	jsonBytes, jsonErr := json.Marshal(result)
